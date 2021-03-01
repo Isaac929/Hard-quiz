2c3a09b7e990efe2fc7af649fd854a901efad76a
Isaac <79817192+Isaac929@users.noreply.github.com>
2021-02-28 18:23:10 -0800

    Add files via upload

---
+++ MY QUIZ.py
@@ -0,0 +1,53 @@
+print ('Oh hi I thought I would make a quiz, because coding is fun. So I did, good luck')
+print ('P.S. IF YOU THINK YOU WILL GET 100% YOU ARE 100% WRONG MWAHAHAHAHAHAHAHA!')
+print ('Unless you cheat')
+print ('Ok sorry I have a BIG mouth')
+print ('we should get started')
+
+ans = input ('Are you ready to play (yes/no): ')
+
+if ans.lower() == 'yes' or ans.lower() == 'y' or ans.lower() == 'ye':
+    ans = input ('1. How many stars are on the national flag of USA? In numbers ONLY')
+    if '50':
+        print('Correct')
+    else:
+        print('Incorrect')
+
+    ans = input ('2. What is Joe Bidens middle name? Middle name ONLY.')
+    if ans.lower() == 'robinette':
+        print('Correct')
+    else:
+        print('Incorrect')
+
+    ans = input ('3. How many breeds of elephant are there? In numbers ONLY.')
+    if '3':
+        print('Correct')
+    else:
+        print('Incorrect')
+
+    ans = input ('4. Name the longest river in the UK. Name ONLY.')
+    if ans.lower() == 'severn':
+        print('Correct')
+    else:
+        print('Incorrect')
+
+    ans = input ('5. Who designed the Eiffel Tower? First & last name ONLY.')
+    if ans.lower() == 'gustave eiffel':
+        print('Correct')
+    else:
+        print('Incorrect')
+
+    ans = input ('6. What is the spiciest chilli in the world? Name ONLY.')
+    if ans.lower() == 'carolina reaper':
+        print('Correct')
+    else:
+        print('Incorrect')
+
+    ans = input ('7. Who has won the most Academy Awards? First & last name ONLY.')
+    if ans.lower() == 'katharine hepburn':
+        print('Correct')
+    else:
+        print('Incorrect')
+
+        print ('Thanks for playing :D ! I hope you did good, but its one of the hardest quizes so you probably didnt :|')
+        print ('Anyway, byee')
